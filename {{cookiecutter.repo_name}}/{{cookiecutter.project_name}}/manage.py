bf2a75c7df693721fcc3f7f148ba4cd08382e7eb
Saurabh Kumar <thes.kumar@gmail.com>
2013-12-28 19:22:04 +0530

    feat(dotenv): add django-dotenv support.
    manage.py do not read `.env`. Let's fix that.
    
    Usecase:
    While working b/w different project it's becomes difficult to work
    with environment variables. Also we don't want to track the local
    enviroment varibale.
    
    Solution:
    Using django-dotenv[1], we can save all the local environment per
    project in a .env file. This file should not be tracked, instead
    a `.env.sample` should be used to educated the user for possible
    environment variables.
    
    [1] https://github.com/jacobian/django-dotenv

--- {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/manage.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/manage.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 import os
 import sys
+import dotenv
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{cookiecutter.project_name}}.config.settings")
     os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
+
+    dotenv.read_dotenv()
 
     from configurations.management import execute_from_command_line
 
