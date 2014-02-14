c52c4234b95a77b82d214db14db8343ccc3c8c7a
Saurabh Kumar <thes.kumar@gmail.com>
2014-02-15 01:35:38 +0530

    feat(fabfile): add fabfile with basic operations

---
+++ {{cookiecutter.repo_name}}/fabfile.py
@@ -0,0 +1,54 @@
+from fabric.api import *
+
+env.project_name = '{{cookiecutter.project_name}}'
+
+# ==========================================================================
+#  Enviroments
+# ==========================================================================
+
+def staging():
+    env.remote = 'staging'
+    env.branch = 'staging'
+
+def dev():
+    env.remote = 'dev'
+    env.branch = 'dev'
+
+
+# ==========================================================================
+#  Tasks
+# ==========================================================================
+
+def setup():
+    local('pip install -r requirements/local.txt')
+    manage('syncdb')
+    manage('migrate')
+    manage('createsuperuser')
+
+
+def manage(cmd):
+    local('python {project_name}/manage.py {cmd}'.format(
+                            project_name=env.project_name, cmd=cmd))
+
+def test():
+    manage('test %(project_name)s' % env)
+
+
+def shell():
+    manage('shell')
+
+
+def serve(address='127.0.0.1:8000'):
+    '''Start local development server'''
+    local('pip install -r requirements/local.txt')
+    manage('runserver %s' % address)
+
+
+def deploy():
+    test()
+    local('git push origin %(branch)s' % env)
+    local('git push %(remote)s %(branch)s:master' % env)
+
+
+def setup_mac():
+    local('brew install libmemcached')
